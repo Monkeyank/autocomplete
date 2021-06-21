49906e4383ffd9ec7a018404832278622a7b318e
Ankith Madadi <ankithm28@gmail.com>
2021-06-21 13:22:41 -0700

    stash

--- dev/django.ts
+++ dev/django.ts
@@ -1,21 +1,31 @@
 export const completion: Fig.Spec = {
   name: "django",
   description: "Django Commands",
-  generateSpec: async (context, executeShellCommand) => {
+  generateSpec: async (contex, executeShellCommand) => {
     var out = await executeShellCommand("django-admin help --commands");
-    var formatted = out.split("\n");
-    const subcommands = [];
 
-    out.split("\n").map((command) => {
-      subcommands.push({
-        name: `${command}`,
-        description: "jklsfjd",
-      });
-    });
     return {
-      name: `django`,
-      subcommands: [subcommands],
-      description: `${subcommands}`,
+      name: "django",
+
+      description: "bob",
     };
   },
 };
+
+// generateSpec: async (context, executeShellCommand) => {
+//   var out = await executeShellCommand("django-admin help --commands");
+//   var formatted = out.split("\n");
+//   const subcommands = [];
+
+//   out.split("\n").map((command) => {
+//     subcommands.push({
+//       name: `${command}`,
+//       description: "jklsfjd",
+//     });
+//   });
+//   return {
+//     name: `django`,
+//     subcommands: [subcommands],
+//     description: `${subcommands}`,
+//   };
+// },
