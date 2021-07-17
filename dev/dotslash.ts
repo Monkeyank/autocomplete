c01331d5e63839604ab365a2d092a3c79bb651e8
Ankith Madadi <40261054+Monkeyank@users.noreply.github.com>
2021-07-17 15:04:50 -0700

    experimenting

--- dev/dotslash.ts
+++ dev/dotslash.ts
@@ -1,19 +1,19 @@
 const dotslashGenerator: Fig.Generator = {
   script: function (context) {
     let path = context[context.length - 1];
-
-    if (path.startsWith("/") && path.includes("/")) {
-      const components = path.split("/");
-      components.pop();
-      path = "/" + components.join("/");
+    if (path === "/") {
     } else if (path.includes("/")) {
       const components = path.split("/");
+      console.log(components);
       components.pop();
+      console.log(components);
+      if (components[0] === "") {
+        components[0] = "/";
+      }
       path = components.join("/");
     } else {
       path = ".";
     }
-
     // escape spaces in path
     path = path.split(" ").join("\\");
     // output full filepath for each item in `path` folder
@@ -26,15 +26,9 @@ const dotslashGenerator: Fig.Generator = {
       let filename = components[components.length - 1];
       const isExecutable = path.endsWith("*");
       const isFolder = path.endsWith("/");
-      let ext = "";
-
-      try {
-        ext = path.split(".").slice(-1)[0];
-      } catch (e) {}
 
       if (isFolder) {
         filename = components[components.length - 2] + "/";
-        ext = "folder";
       }
 
       // Removes character at the end of the filepath
@@ -42,12 +36,14 @@ const dotslashGenerator: Fig.Generator = {
         filename = filename.slice(0, -1);
       }
 
+      // var temp_path = this.currentWorkingDirectoryForFileAndFolderSuggestions ?? "";
+      // temp_path = temp_path.startsWith("~") ? this.home + temp_path.slice(1) : temp_path;
+
+      // temp_path === "/" ? "" : temp_path.startsWith("//") ? temp_path.slice(1) : temp_path;
       return {
         name: filename,
-        icon: isFolder
-          ? "fig://" + path + `?type=${ext}`
-          : "fig:///" + filename,
-        description: isFolder ? "folder" : "file",
+        icon: "fig:///" + path,
+        description: `${components}`,
         priority: isExecutable && 80,
       };
     });
@@ -64,3 +60,12 @@ export const completionSpec: Fig.Spec = {
     generators: [dotslashGenerator],
   },
 };
+
+// export const completionSpec: Fig.Spec = {
+//   name: "dotslash",
+//   args: {
+//     // This was previously just "filepaths", however, we added folders so
+//     // users of ohmyzsh could cd into a folder by typing the folder name without `cd`
+//     template: ["filepaths", "folders"]
+//   },
+// };
